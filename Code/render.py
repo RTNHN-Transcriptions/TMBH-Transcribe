e0d849d96137ac385272379c1450e9cb41a99ed3
Zach Strout <zachstrout@gmail.com>
2023-04-27 20:06:00 -0500

    Update scripts
    I wanted to make the code more clean and align with typical python
    standards.

--- Code/render.py
+++ Code/render.py
@@ -2,23 +2,28 @@ from jinja2 import Environment, FileSystemLoader
 import json
 import os
 from tqdm import tqdm
-env = Environment(loader=FileSystemLoader('.'))
 
-template = env.get_template('Code/template.html')
+def main():
+    env = Environment(loader=FileSystemLoader('.'))
 
-for filename in tqdm(os.listdir("Data")):
-    
-    with open(f"Data/{filename}", "r") as f:
-        data = json.loads(f.read())
+    template = env.get_template('Code/template.html')
 
-    audio_file = data["meta_data"]["url"]
-    title = data["meta_data"]["title"]
+    for filename in tqdm(os.listdir("Data")):
+        
+        with open(f"Data/{filename}", "r") as f:
+            data = json.loads(f.read())
 
-    transcript_sections = [{"start":segment["start"], "end":segment["end"], "text":segment["text"]} for segment in data["transcription_data"]["segments"] ]
+        audio_file = data["meta_data"]["url"]
+        title = data["meta_data"]["title"]
 
-    # Render the template with the given data
-    rendered_html = template.render(audio_file=audio_file, transcript_sections=transcript_sections, title=title)
-    filename_no_ext = os.path.splitext(filename)[0]
-    # Save the rendered HTML to a file
-    with open(f'SmartTranscripts/{filename_no_ext}.html', 'w') as output_file:
-        output_file.write(rendered_html)
+        transcript_sections = [{"start":segment["start"], "end":segment["end"], "text":segment["text"]} for segment in data["transcription_data"]["segments"] ]
+
+        # Render the template with the given data
+        rendered_html = template.render(audio_file=audio_file, transcript_sections=transcript_sections, title=title)
+        filename_no_ext = os.path.splitext(filename)[0]
+        # Save the rendered HTML to a file
+        with open(f'SmartTranscripts/{filename_no_ext}.html', 'w') as output_file:
+            output_file.write(rendered_html)
+
+if __name__ == "__main__":
+    main()
--- Code/script.py
+++ Code/script.py
@@ -7,72 +7,76 @@ import json
 import torch
 
 
-RSS_LINK = "https://thetenminutebiblehourpodcast.libsyn.com/rss"
-CHECK_LIMIT = 20 
-#There are a lot of problematic episodes, so it will only
-#Check the latest 20 episodes in case I get behind 
 
-RSS_doc = requests.get(RSS_LINK)
-soup = Soup(RSS_doc.content, features="xml")
-episodes = soup.find_all("item") #item is the xml tag for a podcast episode
+def main(RSS_link, check_limit):
+    RSS_doc = requests.get(RSS_LINK)
+    soup = Soup(RSS_doc.content, features="xml")
+    episodes = soup.find_all("item") #item is the xml tag for a podcast episode
 
-if torch.cuda.is_available():
-    model = whisper.load_model("base.en", "cuda")
-else:
-    model = whisper.load_model("base.en")
-#pbar is a reference to the progress bar object so that the description of
-#the current task within the look can be updated. 
-pbar = tqdm(episodes)
-for index, episode in enumerate(pbar):
-    if index > 20:
-        continue
-    try:
-        title = episode.title.text
-        episode_number = title[:title.find("-")-1]
-        pbar.set_description(f"Working on {episode_number}")
-        if exists(f"Data/{episode_number}.json"):
-            print(f"Already Transcribed {episode_number}")
+    if torch.cuda.is_available():
+        model = whisper.load_model("base.en", "cuda")
+    else:
+        model = whisper.load_model("base.en")
+    #pbar is a reference to the progress bar object so that the description of
+    #the current task within the look can be updated. 
+    pbar = tqdm(episodes)
+    for index, episode in enumerate(pbar):
+        if index > 20:
             continue
-        title_text = title[title.find("-")+1:].strip()
-        pub_date = episode.pubDate.text
-        description = episode.description.text
-        url = episode.enclosure["url"]
-        # Sometimes there is extra stuff at the end of the URL
-        if url[-3:] != "mp3":
-            url = url[:url.find(".mp3?")+4]
-        duration = episode.find("itunes:duration").text
-        subtitle = episode.find("itunes:subtitle").text
-        pbar.set_description(f"Working on {episode_number} - Fetching audio")
-        audio = requests.get(url)
-        pbar.set_description(f"Working on {episode_number} - Writing audio to file")
-        with open("tmp.mp3", "wb") as f:
-            f.write(audio.content)
-        pbar.set_description(f"Working on {episode_number} - Transcribing Audio")
-        result = model.transcribe("tmp.mp3", verbose=False)
-        # pbar.set_description(f"Working on {episode_number} - Writing Transcript")
-        # with open(f"Transcripts/{episode_number}.md", "w+", encoding="utf8") as f:
-        #     f.write("---\n")
-        #     f.write(f"title: {title_text}\n")
-        #     f.write(f"episode_num: {episode_number}\n")
-        #     f.write(f"pub_date: {pub_date}\n")
-        #     f.write(f"duration: {duration}\n")
-        #     f.write(f"subtitle: {subtitle}\n")
-        #     f.write(f"url: {url}\n")
-        #     f.write("---\n\n")
-        #     f.write(result["text"])
-        meta_data = {"title":title_text,
-                    "episode_num":episode_number,
-                    "pub_date":pub_date,
-                    "duration": duration,
-                    "subtitle": subtitle,
-                    "url": url}
-        all_data = {"meta_data": meta_data, "transcription_data": result, "rss_data":str(episode)}
-        with open(f"Data/{episode_number}.json", "w+", encoding="utf8") as f:
-            f.write(json.dumps(all_data))
-    except KeyboardInterrupt:
-        raise
-    except Exception as e:
-        title = episode.title.text
-        print(f'There was a problem with the episode {title}')
-        print(e)
+        try:
+            title = episode.title.text
+            episode_number = title[:title.find("-")-1]
+            pbar.set_description(f"Working on {episode_number}")
+            if exists(f"Data/{episode_number}.json"):
+                print(f"Already Transcribed {episode_number}")
+                continue
+            title_text = title[title.find("-")+1:].strip()
+            pub_date = episode.pubDate.text
+            description = episode.description.text
+            url = episode.enclosure["url"]
+            # Sometimes there is extra stuff at the end of the URL
+            if url[-3:] != "mp3":
+                url = url[:url.find(".mp3?")+4]
+            duration = episode.find("itunes:duration").text
+            subtitle = episode.find("itunes:subtitle").text
+            pbar.set_description(f"Working on {episode_number} - Fetching audio")
+            audio = requests.get(url)
+            pbar.set_description(f"Working on {episode_number} - Writing audio to file")
+            with open("tmp.mp3", "wb") as f:
+                f.write(audio.content)
+            pbar.set_description(f"Working on {episode_number} - Transcribing Audio")
+            result = model.transcribe("tmp.mp3", verbose=False)
+            # pbar.set_description(f"Working on {episode_number} - Writing Transcript")
+            # with open(f"Transcripts/{episode_number}.md", "w+", encoding="utf8") as f:
+            #     f.write("---\n")
+            #     f.write(f"title: {title_text}\n")
+            #     f.write(f"episode_num: {episode_number}\n")
+            #     f.write(f"pub_date: {pub_date}\n")
+            #     f.write(f"duration: {duration}\n")
+            #     f.write(f"subtitle: {subtitle}\n")
+            #     f.write(f"url: {url}\n")
+            #     f.write("---\n\n")
+            #     f.write(result["text"])
+            meta_data = {"title":title_text,
+                        "episode_num":episode_number,
+                        "pub_date":pub_date,
+                        "duration": duration,
+                        "subtitle": subtitle,
+                        "url": url}
+            all_data = {"meta_data": meta_data, "transcription_data": result, "rss_data":str(episode)}
+            with open(f"Data/{episode_number}.json", "w+", encoding="utf8") as f:
+                f.write(json.dumps(all_data))
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            title = episode.title.text
+            print(f'There was a problem with the episode {title}')
+            print(e)
+
+if __name__ == "__main__":
+    RSS_LINK = "https://thetenminutebiblehourpodcast.libsyn.com/rss"
+    CHECK_LIMIT = 20 
+    #There are a lot of problematic episodes, so it will only
+    #Check the latest 20 episodes in case I get behind 
+    main(RSS_LINK, CHECK_LIMIT)
     
